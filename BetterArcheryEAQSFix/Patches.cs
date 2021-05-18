187c4c04eb2c0bb294291358985c92f804c16da9
jsza <jayess@jayess.za.net>
2021-05-19 00:55:54 +0200

    Better Archery EAQS Fix 0.0.1

---
+++ BetterArcheryEAQSFix/Patches.cs
@@ -0,0 +1,144 @@
+﻿using System;
+
+using HarmonyLib;
+
+using BA = BetterArchery.BetterArchery;
+
+
+namespace BetterArcheryEAQSFix
+{
+    // protected override void Awake()
+    [HarmonyPatch(typeof(Player), nameof(Player.Awake))]
+    public static class Player_Awake_Patch
+    {
+        public static void Postfix(Player __instance)
+        {
+            BetterArcheryState.UpdateRowIndex();
+        }
+    }
+
+    // public void Load(ZPackage pkg)
+    [HarmonyPatch(typeof(Player), nameof(Player.Load))]
+    public static class Player_Load_Patch
+    {
+        public static bool loading = false;
+
+        public static void Prefix()
+        {
+            loading = true;
+        }
+
+        public static void Postfix(Player __instance)
+        {
+            loading = false;
+
+            // Avoid trying to drop items at character select screen
+            if (__instance != Player.m_localPlayer)
+            {
+                return;
+            }
+
+            Inventory inventory = __instance.m_inventory;
+            Plugin.logger.LogInfo($"Searching player inventory for lost items.");
+            for (int i = inventory.m_inventory.Count - 1; i >= 0; i--)
+            {
+                ItemDrop.ItemData itemData = inventory.m_inventory[i];
+                Vector2i pos = itemData.m_gridPos;
+                if (
+                    pos.y >= BetterArcheryState.RowStartIndex
+                    && pos.y <= BetterArcheryState.RowEndIndex
+                    && !(BA.IsQuiverSlot(pos) && itemData.m_shared.m_itemType == ItemDrop.ItemData.ItemType.Ammo)
+                )
+                {
+                    Plugin.logger.LogWarning($"Found {itemData.m_shared.m_name} x {itemData.m_stack} in Better Archery slots; attempting to drop.");
+                    __instance.DropItem(inventory, itemData, itemData.m_stack);
+                }
+            }
+        }
+    }
+
+    // private void OnTakeAll()
+    [HarmonyPatch(typeof(InventoryGui), nameof(InventoryGui.OnTakeAll))]
+    public static class InventoryGui_OnTakeAll_Patch
+    {
+        public static bool takingAllTombstone = false;
+
+        public static void Prefix(InventoryGui __instance)
+        {
+            Container container = __instance.m_currentContainer;
+            if (container != null && container.GetComponent<TombStone>())
+            {
+                takingAllTombstone = true;
+            }
+        }
+
+        public static void Postfix()
+        {
+            takingAllTombstone = false;
+        }
+    }
+
+    // public bool Interact(Humanoid character, bool hold)
+    [HarmonyPatch(typeof(TombStone), nameof(TombStone.Interact))]
+    public static class TombStone_Interact_Patch
+    {
+        public static bool interactingTombstone = false;
+
+        public static void Prefix()
+        {
+            interactingTombstone = true;
+        }
+
+        public static void Postfix()
+        {
+            interactingTombstone = false;
+        }
+    }
+
+    // public int GetEmptySlots()
+    [HarmonyPatch(typeof(Inventory), nameof(Inventory.GetEmptySlots))]
+    public static class Inventory_GetEmptySlots_Patch
+    {
+        public static void Postfix(Inventory __instance, ref int __result, bool __runOriginal)
+        {
+            if (!BetterArcheryState.QuiverEnabled)
+            {
+                return;
+            }
+
+            if (TombStone_Interact_Patch.interactingTombstone && __instance.GetName() == "Inventory")
+            {
+                // Better Archery adds 2 (hidden from UI) rows whose slots we need to ignore
+                // Prevents trying to move items to Better Archery's hidden inventory when interacting with tombstone
+                // (TombStone.EasyFitInInventory calls GetEmptySlots)
+                int subtractSlots = __instance.m_width * BetterArcheryState.RowCount;
+                __result = Math.Max(0, __result - subtractSlots);
+                Plugin.logger.LogWarning($"GetEmptySlots: subtracted {subtractSlots} BetterArchery slots (now {__result})");
+            }
+        }
+    }
+
+    // private bool AddItem(ItemDrop.ItemData item, int amount, int x, int y)
+    [HarmonyPatch(typeof(Inventory), nameof(Inventory.AddItem), new[] { typeof(ItemDrop.ItemData), typeof(int), typeof(int), typeof(int) })]
+    public static class Inventory_AddItem_Patch
+    {
+        public static bool Prefix(ItemDrop.ItemData item, int amount, int x, int y, ref bool __result)
+        {
+            if (!BetterArcheryState.QuiverEnabled)
+            {
+                return true;
+            }
+
+            if (InventoryGui_OnTakeAll_Patch.takingAllTombstone || TombStone_Interact_Patch.interactingTombstone)
+            {
+                if (y >= BetterArcheryState.RowStartIndex && y <= BetterArcheryState.RowEndIndex)
+                {
+                    Plugin.logger.LogWarning($"Blocking Inventory.Additem {item.m_shared.m_name} x {amount} into Better Archery slot {x},{y}.");
+                    __result = false;
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ BetterArcheryEAQSFix/Plugin.cs
@@ -0,0 +1,57 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+
+using HarmonyLib;
+
+
+namespace BetterArcheryEAQSFix
+{
+    [BepInPlugin("MVP.BetterArcheryEAQSFix", "BetterArcheryEAQSFix", "0.0.1")]
+    [BepInDependency("ishid4.mods.betterarchery")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static ManualLogSource logger;
+
+        private void Awake()
+        {
+            logger = this.Logger;
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    public static class BetterArcheryState
+    {
+        public const int RowCount = 2;
+        public static bool QuiverEnabled = false;
+        public static int RowStartIndex = 0;
+        public static int RowEndIndex = 0;
+
+        public static int GetRowStartIndex()
+        {
+            return BetterArchery.BetterArchery.QuiverRowIndex - 1;
+        }
+
+        public static int GetRowEndIndex()
+        {
+            return BetterArchery.BetterArchery.QuiverRowIndex - 1;
+        }
+
+        public static void UpdateRowIndex()
+        {
+            int QuiverRowIndex = BetterArchery.BetterArchery.QuiverRowIndex;
+            if (QuiverRowIndex > 0)
+            {
+                QuiverEnabled = true;
+                RowStartIndex = QuiverRowIndex - 1;
+                RowEndIndex = RowStartIndex + RowCount;
+            }
+            else
+            {
+                QuiverEnabled = false;
+                RowStartIndex = 0;
+                RowEndIndex = 0;
+            }
+        }
+    }
+}
